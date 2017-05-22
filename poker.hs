4129c4149dba6ff372c1278aaadf666221c00101
Fabian Bull <fabian.bull@bioinform.at>
2017-05-22 21:24:36 +0200

    Add ordering of high card ranking.

--- poker.hs
+++ poker.hs
@@ -1,8 +1,8 @@
 -- Model of Texas Holdem
 
-data Suit = Clubs | Spades | Hearts | Diamonds deriving (Enum, Eq, Show)
+data Suit = Clubs | Spades | Hearts | Diamonds deriving (Enum, Ord, Eq, Show)
 
-data Value = Ace | King | Queen | Jack | Ten | Nine | Eight | Seven | Six | Five | Four | Three | Two deriving (Enum, Eq, Show)
+data Value = Ace | King | Queen | Jack | Ten | Nine | Eight | Seven | Six | Five | Four | Three | Two deriving (Enum, Ord, Eq, Show)
 
 data Card = Card Suit Value deriving (Eq, Show)
 
@@ -17,7 +17,8 @@ values = [Ace .. Two]
 deck :: Cards
 deck = [ Card s v | s <- suits, v <- values]
 
-data Rank = HighCard Value Value Value Value Value | -- All 5 cards
+-- All values should be present in sorted order
+data Rank = HighCard Value Value Value Value Value | -- All 5 cards 
     Pair Value Value Value Value | -- The pair and 3 kickers
     TwoPair Value Value Value | -- Two pairs a kicker
     ThreeOfAKind Value Value Value | -- The tripple and two kickers
@@ -26,3 +27,11 @@ data Rank = HighCard Value Value Value Value Value | -- All 5 cards
     FullHouse  Value Value | -- Tripples and pair
     FourOfAKind Value Value | -- Quadruple and a kicker
     StraightFlush Value -- High card of straight and flush
+    deriving (Eq, Show)
+
+instance Ord Rank where
+
+    compare (HighCard a1 a2 a3 a4 a5) (HighCard b1 b2 b3 b4 b5) = compare [a1, a2, a3, a4, a5] [b1, b2, b3, b4, b5]
+    compare (HighCard _ _ _ _ _) _ = LT
+    compare _ (HighCard _ _ _ _ _) = GT
+
