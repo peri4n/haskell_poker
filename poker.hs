89ec06d5b801ff25b00004d7fdb561ffcef58732
Fabian Bull <fabian.bull@bioinform.at>
2017-05-22 21:11:42 +0200

    Add hand ranking model

--- poker.hs
+++ poker.hs
@@ -1,3 +1,4 @@
+-- Model of Texas Holdem
 
 data Suit = Clubs | Spades | Hearts | Diamonds deriving (Enum, Eq, Show)
 
@@ -15,3 +16,13 @@ values = [Ace .. Two]
 
 deck :: Cards
 deck = [ Card s v | s <- suits, v <- values]
+
+data Rank = HighCard Value Value Value Value Value | -- All 5 cards
+    Pair Value Value Value Value | -- The pair and 3 kickers
+    TwoPair Value Value Value | -- Two pairs a kicker
+    ThreeOfAKind Value Value Value | -- The tripple and two kickers
+    Straight Value | -- High card of the straigt
+    Flush Value | -- High card of the flush
+    FullHouse  Value Value | -- Tripples and pair
+    FourOfAKind Value Value | -- Quadruple and a kicker
+    StraightFlush Value -- High card of straight and flush
